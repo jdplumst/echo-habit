1cc2d291cf1180659b6928713bd59c87bbc1d171
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-06-16 20:29:10 -0400

    Set up react router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Routes } from "react-router";
+import { Route } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/pomodoro" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
