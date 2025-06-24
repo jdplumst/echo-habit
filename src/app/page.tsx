f670023a922dfb1e01808e4d58836ebaeb74c358
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-06-23 21:43:09 -0400

    Add continue without an account button to sign in component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
-import Link from "next/link";
 import { SignIn } from "~/components/sign-in";
-import { Button } from "~/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="min-w-screen flex min-h-screen flex-col items-center gap-4 bg-gradient-to-b from-[#00D4FF] to-[#090979] p-8">
-      <h1 className="text-7xl font-bold">Echo Habit</h1>
-      <h2 className="text-xl font-semibold">
+    <div className="min-w-screen flex min-h-screen flex-col items-center gap-14 bg-gradient-to-b from-[#00D4FF] to-[#090979] p-8">
+      <h1 className="text-center text-7xl font-bold">Echo Habit</h1>
+      <h2 className="text-center text-xl font-semibold">
         Start being more productive using pomodoro technique and track your
         progress!
       </h2>
-      <Button>
-        <Link href="/timer">Start tracking!</Link>
-      </Button>
       <SignIn />
     </div>
   );
--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -5,6 +5,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { useState } from "react";
 import { signIn } from "~/lib/auth-client";
 import { cn } from "~/lib/utils";
+import Link from "next/link";
 
 export function SignIn() {
   const [loading, setLoading] = useState(false);
@@ -12,17 +13,20 @@ export function SignIn() {
   return (
     <Card className="max-w-md">
       <CardHeader>
-        <CardTitle className="text-lg md:text-xl">Sign In</CardTitle>
+        <CardTitle className="text-center text-lg md:text-xl">
+          Sign In
+        </CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="grid gap-4">
-          <div
-            className={cn(
-              "flex w-full items-center gap-2",
-              "flex-col justify-between",
-            )}
-          >
-            {/* <Button
+        <div className="grid gap-14">
+          <div className="grid gap-4">
+            <div
+              className={cn(
+                "flex w-full items-center gap-2",
+                "flex-col justify-between",
+              )}
+            >
+              {/* <Button
               variant="outline"
               className={cn("w-full gap-2")}
               disabled={loading}
@@ -68,40 +72,50 @@ export function SignIn() {
               </svg>
               Sign in with Google
             </Button> */}
-            <Button
-              variant="outline"
-              className={cn("w-full gap-2")}
-              disabled={loading}
-              onClick={async () => {
-                await signIn.social(
-                  {
-                    provider: "discord",
-                    callbackURL: "/timer",
-                    errorCallbackURL: "/error",
-                  },
-                  {
-                    onRequest: () => {
-                      setLoading(true);
+              <Button
+                variant="outline"
+                className={cn("w-full gap-2")}
+                disabled={loading}
+                onClick={async () => {
+                  await signIn.social(
+                    {
+                      provider: "discord",
+                      callbackURL: "/timer",
+                      errorCallbackURL: "/error",
                     },
-                    onResponse: () => {
-                      setLoading(false);
+                    {
+                      onRequest: () => {
+                        setLoading(true);
+                      },
+                      onResponse: () => {
+                        setLoading(false);
+                      },
                     },
-                  },
-                );
-              }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="1em"
-                height="1em"
-                viewBox="0 0 24 24"
+                  );
+                }}
               >
-                <path
-                  fill="currentColor"
-                  d="M19.27 5.33C17.94 4.71 16.5 4.26 15 4a.1.1 0 0 0-.07.03c-.18.33-.39.76-.53 1.09a16.1 16.1 0 0 0-4.8 0c-.14-.34-.35-.76-.54-1.09c-.01-.02-.04-.03-.07-.03c-1.5.26-2.93.71-4.27 1.33c-.01 0-.02.01-.03.02c-2.72 4.07-3.47 8.03-3.1 11.95c0 .02.01.04.03.05c1.8 1.32 3.53 2.12 5.24 2.65c.03.01.06 0 .07-.02c.4-.55.76-1.13 1.07-1.74c.02-.04 0-.08-.04-.09c-.57-.22-1.11-.48-1.64-.78c-.04-.02-.04-.08-.01-.11c.11-.08.22-.17.33-.25c.02-.02.05-.02.07-.01c3.44 1.57 7.15 1.57 10.55 0c.02-.01.05-.01.07.01c.11.09.22.17.33.26c.04.03.04.09-.01.11c-.52.31-1.07.56-1.64.78c-.04.01-.05.06-.04.09c.32.61.68 1.19 1.07 1.74c.03.01.06.02.09.01c1.72-.53 3.45-1.33 5.25-2.65c.02-.01.03-.03.03-.05c.44-4.53-.73-8.46-3.1-11.95c-.01-.01-.02-.02-.04-.02M8.52 14.91c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.84 2.12-1.89 2.12m6.97 0c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.83 2.12-1.89 2.12"
-                ></path>
-              </svg>
-              Sign in with Discord
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M19.27 5.33C17.94 4.71 16.5 4.26 15 4a.1.1 0 0 0-.07.03c-.18.33-.39.76-.53 1.09a16.1 16.1 0 0 0-4.8 0c-.14-.34-.35-.76-.54-1.09c-.01-.02-.04-.03-.07-.03c-1.5.26-2.93.71-4.27 1.33c-.01 0-.02.01-.03.02c-2.72 4.07-3.47 8.03-3.1 11.95c0 .02.01.04.03.05c1.8 1.32 3.53 2.12 5.24 2.65c.03.01.06 0 .07-.02c.4-.55.76-1.13 1.07-1.74c.02-.04 0-.08-.04-.09c-.57-.22-1.11-.48-1.64-.78c-.04-.02-.04-.08-.01-.11c.11-.08.22-.17.33-.25c.02-.02.05-.02.07-.01c3.44 1.57 7.15 1.57 10.55 0c.02-.01.05-.01.07.01c.11.09.22.17.33.26c.04.03.04.09-.01.11c-.52.31-1.07.56-1.64.78c-.04.01-.05.06-.04.09c.32.61.68 1.19 1.07 1.74c.03.01.06.02.09.01c1.72-.53 3.45-1.33 5.25-2.65c.02-.01.03-.03.03-.05c.44-4.53-.73-8.46-3.1-11.95c-.01-.01-.02-.02-.04-.02M8.52 14.91c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.84 2.12-1.89 2.12m6.97 0c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.83 2.12-1.89 2.12"
+                  ></path>
+                </svg>
+                Sign in with Discord
+              </Button>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-2">
+            <div className="text-center">
+              You can also continue without an account, but your progress
+              won&apos;t be saved.
+            </div>
+            <Button variant="outline">
+              <Link href="/timer">Continue Without an Account</Link>
             </Button>
           </div>
         </div>
