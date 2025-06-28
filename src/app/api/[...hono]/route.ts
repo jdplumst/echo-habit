d408ea0e14449c1d436d678a7952af460ce0ef8f
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-06-28 18:34:12 -0400

    Remoe hono files

--- src/app/api/[...hono]/route.ts
+++
@@ -1,7 +0,0 @@
-import { app } from "~/server/hono";
-import { handle } from "hono/vercel";
-
-export const runtime = "nodejs";
-
-export const GET = handle(app);
-export const POST = handle(app);
--- src/server/routers/auth.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from "hono";
-import { auth } from "~/lib/auth";
-
-export const authRouter = new Hono();
-
-authRouter.on(["POST", "GET"], "/*", (c) => {
-  return auth.handler(c.req.raw);
-});
